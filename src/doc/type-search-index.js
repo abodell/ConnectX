a12a884ba9c93c199c440154dc7fa33ac8fa44e6
Andy Bodell <91170282+abodell@users.noreply.github.com>
2022-05-20 10:00:26 -0400

    Delete src/doc directory

--- src/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"cpsc2150.extendedConnectX.views","l":"ConnectXView"},{"p":"cpsc2150.extendedConnectX.views","l":"SetupView"}]
